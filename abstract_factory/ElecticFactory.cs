bbe4d8f05bbcc1cdcd686b2c750aa94878047161
NB-Florian <NB-Florian@DESKTOP-MU31T7R>
2024-09-13 11:25:18 +0200

    Projektdateien hinzufügen.

---
+++ abstract_factory/ElecticFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+     internal class ElecticFactory : VehicleFactory
+    {
+         public ElecticFactory() { 
+        
+        
+         }
+         public void createCar(Car car){
+
+
+         }
+    
+        public void createBike(Bike bike) { 
+        
+        
+        }
+
+
+    }
+}
---
+++ abstract_factory/NonElectricFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    internal class NonElectricFactory
+    {
+
+        public NonElectricFactory() { 
+        
+        
+        }
+
+        public void createCar(Car car)
+        {
+
+
+        }
+
+        public void createBike(Bike bike)
+        {
+
+
+        }
+    }
+
+}
